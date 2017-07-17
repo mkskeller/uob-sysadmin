45d7351bede1fe27883af2381939f8306d14de1b
Marcel Keller <m.keller@bristol.ac.uk>
2017-07-17 03:36:37 +0200

    Copy flags solely based on message id.

--- copy-the-flags.py
+++ copy-the-flags.py
@@ -4,6 +4,7 @@ import getpass
 import email
 from imapclient import IMAPClient
 import sys
+import collections
 
 print 'Usage: %s <username>' % sys.argv[0]
 
@@ -15,19 +16,19 @@ dest = IMAPClient('outlook.office365.com', ssl=True)
 source.login(user, getpass.getpass('Gmail password: '))
 dest.login(user, getpass.getpass('Uni password: '))
 
+flags = {}
+
+print 'processing source'
 for folder in source.list_folders():
     folder = folder[-1]
     try:
         source.select_folder(folder)
-        dest.select_folder(folder)
     except:
         continue
     print 'processing ', folder
 
-    full_fetch = ['FLAGS', 'INTERNALDATE', 'RFC822']
-
-    batch = 100
-    msgnums = source.search('OR ANSWERED FLAGGED')
+    batch = 1000
+    msgnums = source.search()
     print msgnums
     i = 0
     while msgnums[i:i+batch]:
@@ -36,8 +37,36 @@ for folder in source.list_folders():
             msg = email.message_from_string(data['RFC822.HEADER'])
             mid = msg['message-id']
             print mid, msg['from'], msg['subject']
-            dest.set_flags(msgnum, data['FLAGS'])
+            flags[mid] = data['FLAGS']
         i += batch
+
+print 'processing dest'
+for folder in dest.list_folders():
+    folder = folder[-1]
+    try:
+        dest.select_folder(folder)
+    except:
+        continue
+    print 'processing', folder
+    msgnums = dest.search()
+    print msgnums
+    i = 0
+    batch = 1000
+    to_set = collections.defaultdict(list)
+    while msgnums[i:i+batch]:
+        msgs = source.fetch(msgnums[i:i+batch], ['FLAGS', 'RFC822.HEADER'])
+        for msgnum, data in msgs.iteritems():
+            msg = email.message_from_string(data['RFC822.HEADER'])
+            mid = msg['message-id']
+            if mid in flags:
+                if flags[mid] != data['FLAGS']:
+                    print mid, msg['from'], msg['subject'], \
+                        flags[mid], data['FLAGS']
+                    to_set[tuple(flags[mid])].append(msgnum)
+        i += batch
+    for f,msgnums in to_set.iteritems():
+        print f, msgnums
+        dest.set_flags(msgnums, f)
 
 for conn in source, dest:
     conn.close_folder()
